53755d663bf11187763c7eba16d3a83758ac5723
meyacine <my.meddah@gmail.com>
2017-11-21 17:16:21 +0100

    compose pour le redux devtools

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // Partie propre à REDUX
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import {Provider} from 'react-redux';
 // Partie propre à Redux-observable
 import{createEpicMiddleware} from 'redux-observable';
@@ -16,9 +16,15 @@ import {rootEpic} from "./epics/stories-epics";
 
 const epicMiddleware = createEpicMiddleware(rootEpic);
 
-// je crée un store en partant du reducer
-const store = createStore(reducer, applyMiddleware(epicMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+// je crée un store en partant du reducer
+const store = createStore(
+    reducer,
+    composeEnhancers(
+        applyMiddleware(epicMiddleware)
+    )
+);
 // Je wrap mon App avec le Provider, comme ça tous les components pourront avoir accès au store si j'ai bien tout compris
 ReactDOM.render(
     <Provider store={store}>
