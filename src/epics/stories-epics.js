a96d971b60929df8fd4b9e32098b0d04f544b4f3
meyacine <my.meddah@gmail.com>
2017-11-21 16:51:17 +0100

    ofType of redux-observable

--- src/epics/stories-epics.js
+++ src/epics/stories-epics.js
@@ -5,7 +5,7 @@ import {StoriesActionsEnum} from "../actions/stories-actions";
 
 function loadStoriesEpic(action$) {
     return action$
-        .filter(action => action.type===StoriesActionsEnum.LOAD_DATA)
+        .ofType(StoriesActionsEnum.LOAD_DATA)
         .do(action => console.log(action))
         .ignoreElements();
 }
