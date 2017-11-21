681fb54992feac7c981f930123e1d8bfe654b4ad
meyacine <my.meddah@gmail.com>
2017-11-21 15:31:15 +0100

    bootstrap redux, avec un component

--- src/index.js
+++ src/index.js
@@ -4,5 +4,20 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Partie propre à REDUX
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+// j'importe le StoriesReducer sous le nom de reducer
+import reducer from './reducers/Stories';
+
+// je crée un store en partant du reducer
+const store = createStore(reducer);
+
+// Je wrap mon App avec le Provider, comme ça tous les components pourront avoir accès au store si j'ai bien tout compris
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
 registerServiceWorker();
---
+++ src/reducers/Stories.js
@@ -0,0 +1,10 @@
+const initialState = {
+    items : []
+}
+
+// le reducer prend un state, et une action. Il fait le routage des actions
+export function StoriesReducer(state = initialState, action) {
+    return state;
+}
+
+export default StoriesReducer;
