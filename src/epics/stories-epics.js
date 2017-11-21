6af0d4255330b227f87555418b970e1005fb0f0a
meyacine <my.meddah@gmail.com>
2017-11-21 16:54:32 +0100

    rxjs switchMap

--- src/epics/stories-epics.js
+++ src/epics/stories-epics.js
@@ -1,13 +1,14 @@
 import {Observable} from 'rxjs';
 import {combineEpics} from 'redux-observable';
-import {StoriesActionsEnum} from "../actions/stories-actions";
+import {cancel, StoriesActionsEnum} from "../actions/stories-actions";
 
 
 function loadStoriesEpic(action$) {
     return action$
         .ofType(StoriesActionsEnum.LOAD_DATA)
-        .do(action => console.log(action))
-        .ignoreElements();
+        .switchMap(()=> {
+            return Observable.of(cancel()).delay(500);
+        });
 }
 
 export const rootEpic = combineEpics(loadStoriesEpic);
